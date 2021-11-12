f4aa1e30c7f6c26bffdafe5c60f7232909a996f6
Order <43047529+Orrder@users.noreply.github.com>
2021-11-12 09:38:39 +0300

    Update ChapterReadSession.cs

--- src/MangaDexSharp/Objects/ChapterReadSession.cs
+++ src/MangaDexSharp/Objects/ChapterReadSession.cs
@@ -73,7 +73,9 @@ namespace MangaDexSharp.Objects
                 && MarkAsReadOnClose
                 && !IsClosed)
             {
-                Task.Run(Close);
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Close();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             }
         }
 
